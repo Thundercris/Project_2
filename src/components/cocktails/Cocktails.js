a5e96d50984a0358838fcf9937239585168805c1
Cristina Grande <cri.grande@gmail.com>
2021-06-17 19:53:34 +0200

    letters

--- src/components/cocktails/Cocktails.js
+++ src/components/cocktails/Cocktails.js
@@ -5,12 +5,12 @@ import axios from 'axios'
 const Cocktails = () => {
   const [cocktails, setCocktails] = useState()
   const [hasError, setHasError] = useState(false)
-
+  const alphabetLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s')
+        const response = await axios.get(`www.thecocktaildb.com/api/json/v1/1/search.php?f=${id}`)
         setCocktails(response)
       } catch (err) {
         setHasError(true)
@@ -26,14 +26,18 @@ const Cocktails = () => {
       <div className="hero-body">
         <div className="container">
           <div className="title is-1 has-text-centered">
-            LOOK AT ALL OF THE COCKTAILS!!!!
+            LOOK AT ALL OF THE COCKTAILS!!!! 
+          </div>
+          <div>
+            {alphabetLetters.map(letter => {
+              return <a className="letters" id={letter}>{letter}</a>
+            })
+            }
           </div>
         </div>
       </div>
     </section>
-
   )
 }
 
 export default Cocktails
-
