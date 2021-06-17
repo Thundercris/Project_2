1ba9c4227d6c40d3961de13d305dd9765827a2f7
Cristina Grande <cri.grande@gmail.com>
2021-06-17 23:53:55 +0200

    letters list display

--- src/components/cocktails/Cocktails.js
+++ src/components/cocktails/Cocktails.js
@@ -1,35 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import CocktailCard from './CocktailCard'
 
 
 const Cocktails = () => {
-  const [cocktails, setCocktails] = useState()
-  // const [hasError, setHasError] = useState(false)
-  const alphabetLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  const [cocktailsLetters, setCocktailsLetters] = useState()
-  const [letter, setLetter] = useState('')
-  
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const { data } = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
-        setCocktailsLetters(data.drink)
-      } catch (err) {
-        console.log(err)
-      }
-    getData()
-  }, [])
+  const [cocktails, setCocktails] = useState([])
+  const alphabetLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  const [cocktailsLetters, setCocktailsLetters] = useState([])
+  const [drinkCards, setdrinkCards] = useState()
 
-  const handleClick = (event) => {
-    setLetter(event.target.innerText)  
+
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.get(`www.thecocktaildb.com/api/json/v1/1/search.php?f=$`)
+  //       setCocktails(response)
+  //     } catch (err) {
+  //       setHasError(true)
+  //     }
+  //   }
+  //   getData()
+  // }, [])
+
+  const handleClick = async (event) => {
+    // console.log(event.target.innerText)
+    try {
+      const { data } = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${event.target.innerText}`)
+      setCocktailsLetters(data.drinks)
+      setdrinkCards(data.drinks[0].strDrink)
+    } catch (err) {
+      console.log(err)
+    }
+
   }
-  
-  console.log()
-  console.log(cocktailsLetters)
+  console.log('drinkCards', drinkCards)
+  console.log('cocktailLetters', cocktailsLetters)
 
-  console.log('cocktails', cocktails)
+  // console.log('cocktails', cocktails)
   return (
     <section className="hero is-fullheight-with-navbar is-primary">
       <div className="hero-body">
@@ -37,18 +45,21 @@ const Cocktails = () => {
           <div>
             {alphabetLetters.map(letter => {
               return <a onClick={handleClick} className="letters" key={letter}>{letter}  </a>
-            }) 
-            }
-          </div>
-          <div className="columns">
-            {cocktailsLetters.map(cocktail => {
-              return <CocktailCard key={cocktail.idDrink} {...cocktail} />
             })
             }
           </div>
+
+          <div className="columns is-multiline">
+            {cocktailsLetters.map(item => {
+              return <div className="column is-one-quarter-desktop is-one-third-tablet">
+                <p>{item.strDrink}</p>
+                <img src={item.strDrinkThumb} />
+              </div>
+            })}
+          </div>
         </div>
       </div>
-    </section>
+    </section >
   )
 }
 
